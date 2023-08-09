b5e4d5f9ad90ca37c5d2c653fe1145100be839ee
Edward Saunders <ed.saunders@sectorglobal.com>
2023-08-09 09:57:11 +0100

    sum of multiple c

---
+++ c/sum-of-multiples/sum_of_multiples.c
@@ -0,0 +1,16 @@
+#include "sum_of_multiples.h"
+
+
+unsigned int sum(const unsigned int *factors, const size_t number_of_factors,
+                 const unsigned int limit) {
+   int sum = 0;
+   for (unsigned int i = 1; i < limit; i++) {
+      for (size_t j = 0; j < number_of_factors; j++) {
+         if (i % factors[j] == 0) {
+            sum += i;
+            break;
+         }
+      }
+   }
+   return sum;
+}
